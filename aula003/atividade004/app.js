28ad079b552970e4af454d747086c45d3736ac69
lima <igorrmatheus2005@gmail.com>
2025-11-12 20:39:25 -0300

    Atividade 04

---
+++ aula003/atividade004/app.js
@@ -0,0 +1,8 @@
+let filmes = ['filme01.jpg', 'filme02.jpg', 'filme03.jpg', 'filme04.jpg', 'filme05.jpg', 'filme06.jpg']
+
+let contador= 0
+while(contador <6 ){
+    console.log(contador)
+    document.write("<img src='imagens/" + filmes[contador] + "'>")
+    contador = contador + 1
+}
